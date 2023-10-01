898c800a060dbb3e6d9f02461db9f5543efe3163
Davi J <davijosantos.dj@gmail.com>
2023-10-01 17:48:23 -0300

    Component SignInButton done

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,20 @@
+import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss'
+// import Image from 'next/image';
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-
         {/* imagens ficam sempre na pasta public */}
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active} >Home</a>
-          <a >Posts</a>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
-  )
+  );
 }
